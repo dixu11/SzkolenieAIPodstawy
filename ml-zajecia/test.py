ff1a1946493dfa74ebb939e18c6b7cd2ce164686
Daniel Szlicht <daniel.szlicht25@gmail.com>
2025-12-05 10:16:39 +0100

    srodowisko

---
+++ ml-zajecia/test.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import datasets, model_selection, linear_model, metrics
+
+print('hello')
+
+#Pomoce do instalacji
+
+# https://jupyter.org/try-jupyter/notebooks/?path=Untitled.ipynb
+# 10:00
+# https://github.com/dixu11/SzkolenieAIPodstawy
+# 10:05
+# python --version
+# python -m venv .venv
+# source .venv/bin/activate
+# pip install pandas matplotlib numpy scikit-learn ipykernel
+# Jak skończy to powinno się dać odpalić to w kodzie bez błędów:
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# from sklearn import datasets, model_selection, linear_model, metrics
+# 10:10
+# Jeśli nie widzi pythona to można przez > create env
+
+# a potem w terminali pip install ... powinno działać
+# ścieżka 3 - codespace
+
+# https://github.com/dixu11/SzkolenieAIPodstawy
+# wymaga konta na github klikamy code /  zakładka codespaces
